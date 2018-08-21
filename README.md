96005698011964b2e52d13da2eec6ed97e01f0ed
Dany Marcoux <danymarcoux@gmail.com>
2018-08-22 00:13:44 +0200

    Add ZNC setup

---
+++ README.md
@@ -0,0 +1,32 @@
+# <a href="https://github.com/dmarcoux/znc">dmarcoux/znc</a>
+
+My setup to run ZNC on *openSUSE Leap 15.0* hosted on *Hetzner*
+
+### Setup
+
+*openSUSE* is supported, but not as part of one of the official images. To install
+*openSUSE* on a *Hetzner* server,
+
+1. Deploy a *Hetzner* server with any of the official images using the web
+   interface
+2. Go under the `ISO Images` on the server's page
+3. Mount the `openSUSE Leap 15.0` ISO image
+4. Reboot the server
+5. Install `openSUSE Leap 15.0`
+6. Unmount the ISO image
+7. Copy SSH public key to the server
+   ```
+   ssh-copy-id user@server_ip
+   ```
+7. SSH into the server
+   ```
+   ssh user@server_ip
+   ```
+8. Download the [initialization script](openSUSE-Leap-15.0.sh) to the server
+   ```
+   curl https://raw.githubusercontent.com/dmarcoux/znc/master/openSUSE-Leap-15.0.sh --output init.sh
+   ```
+9. Run the [initialization script](openSUSE-Leap-15.0.sh)
+   ```
+   bash init.sh
+   ```
---
+++ openSUSE-Leap-15.0.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Initialization script for openSUSE Leap 15.0 image on Hetzner
+
+# -e: Exit on commands not found
+# -u: Exit on unset variables
+# -x: Write to standard error a trace for each command after it expands the command and before it executes it
+set -eux
+
+#### Parameters ####
+
+USER="$(whoami)"
+ZNC_VERSION=1.7.1
+
+echo 'Port to use for ZNC?'
+read -r PORT
+
+##### Updates & Packages #####
+
+# Refresh repositories
+sudo zypper refresh
+
+# Update packages
+sudo zypper update --no-confirm
+
+# Install packages
+sudo zypper install --no-confirm docker fail2ban
+
+##### Setup SSH #####
+
+# Disallow root logins over SSH
+sudo sed --in-place --regexp-extended "s/#?PermitRootLogin .*/PermitRootLogin no/" /etc/ssh/sshd_config
+
+# Disable SSH password authentication
+sudo sed --in-place --regexp-extended "s/#?PasswordAuthentication .*/PasswordAuthentication no/" /etc/ssh/sshd_config
+
+# Restart the SSH service to load the new configuration
+sudo systemctl restart sshd
+
+##### Setup Fail2ban #####
+
+# Enable and start the Fail2ban service
+sudo systemctl enable --now fail2ban
+
+# Setup Fail2ban configuration
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+##### Setup Docker #####
+
+# Add the user to the docker group
+sudo usermod -aG docker "$USER"
+
+# Enable and start the Docker service
+sudo systemctl enable --now docker
+
+# Generate the basic ZNC configuration (it will be configured later through the UI)
+docker run --volume znc-data:/znc-data znc:"$ZNC_VERSION" --makeconf
+
+##### Setup Firewall #####
+
+sudo firewall-cmd --permanent --add-port "$PORT"/tcp
+
+sudo systemctl restart firewalld
+
+##### Run ZNC #####
+
+docker run --detach --publish "$PORT:$PORT" --volume znc-data:/znc-data znc:"$ZNC_VERSION"
